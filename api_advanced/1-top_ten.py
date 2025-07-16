bfbd5beeae684605ac2d539d90fe80d10bfc1f76
Julia04-rub <j.rubibi@alustudent.com>
2025-07-16 13:00:23 +0000

    updated

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -3,21 +3,15 @@
 
 import requests
 
+
 def top_ten(subreddit):
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
-
     response = requests.get(url, headers=headers, allow_redirects=False)
     if not response.ok:
-        return None  # <-- Fix 1: return None (not print)
-
+        print(None)
+        return
     json_data = response.json()
-    posts = json_data.get('data', {}).get('children', [])
-
-    if not posts:
-        return None  # <-- Optional: handle empty subreddit
-
-    for post in posts[:10]:
-        print(post['data']['title'])  # <-- Print top 10 titles
-
-    return 1  # <-- Fix 2: return something meaningful (e.g., 1 or True)
+    posts = json_data['data']['children']
+    for i in range(min(10, len(posts))):
+        print(posts[i]['data']['title'])
