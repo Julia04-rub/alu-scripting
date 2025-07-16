7250f68e889abb65a438ad24dc98dc6c009cc256
Julia04-rub <j.rubibi@alustudent.com>
2025-07-16 13:04:27 +0000

    updted

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-""" return top ten"""
+"""Return top 10 hot post titles for a subreddit"""
 
 import requests
 
-
 def top_ten(subreddit):
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "Mozilla/5.0"}
+
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if not response.ok:
-        print(None)
-        return
-    json_data = response.json()
-    posts = json_data['data']['children']
-    for i in range(min(10, len(posts))):
-        print(posts[i]['data']['title'])
+
+    if response.status_code != 200:
+        return None  # Required when subreddit is invalid
+
+    data = response.json().get("data", {}).get("children", [])
+
+    for post in data[:10]:
+        print(post.get("data", {}).get("title"))
+
+    return "OK"  # <- Required when subreddit is valid
+
